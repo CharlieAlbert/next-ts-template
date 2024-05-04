b4d812952cbd4ec0b7b2a3f58a0ea35d90c8e5c1
CharlieAlbert <charlieraph36@gmail.com>
2024-05-04 22:11:16 +0300

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <section className='p-24'>
+        <div className='container'>
+          <h1 className='text-3xl font-bold'>Next TS Starter...</h1>
+        </div>
+      </section>
+    </div>
+  )
+}
+
+export default page
